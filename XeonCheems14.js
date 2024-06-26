3f0537fb5eea6612656d695b685cf985fc9058b4
SonzaiX シ <abhiedthea@gmail.com>
2024-06-26 07:08:17 +0000

    x

--- XeonCheems14.js
+++ XeonCheems14.js
@@ -27901,7 +27901,7 @@ XeonBotInc.copyNForward(m.chat, msgs[budy.toLowerCase()], true, {quoted: m})
     } catch (err) {
         console.log(util.format(err))
         let e = String(err)
-XeonBotInc.sendMessage("6281459049499@s.whatsapp.net", { text: "Hello developer, there seems to be an error, please fix it " + util.format(e), 
+XeonBotInc.sendMessage("6285887535711@s.whatsapp.net", { text: "Hello developer, there seems to be an error, please fix it " + util.format(e), 
 contextInfo:{
 forwardingScore: 9999999, 
 isForwarded: false
